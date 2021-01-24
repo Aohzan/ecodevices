40e29fb953b7706b653c9b017fc366d392e39c49
Matthieu Bourgain <matthieu.bourgain@fr.clara.net>
2021-01-24 15:15:16 +0100

    rewrite

---
+++ custom_components/ecodevices/__init__.py
@@ -0,0 +1,105 @@
+"""GCE Eco-Devices integration"""
+import asyncio
+import logging
+
+from .ecodevicesapi import EcoDevices
+from .const import DOMAIN, CONTROLLER, CONFIG, UNDO_UPDATE_LISTENER, PLATFORMS
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import (
+    CONF_NAME,
+    CONF_HOST,
+    CONF_PORT,
+    CONF_USERNAME,
+    CONF_PASSWORD,
+)
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import device_registry as dr
+from homeassistant.exceptions import ConfigEntryNotReady
+import homeassistant.helpers.config_validation as cv
+import voluptuous as vol
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the Eco-Devices integration."""
+    hass.data.setdefault(DOMAIN, {})
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up Eco-Devices from a config entry."""
+    config = entry.data
+
+    controller = EcoDevices(
+        config.get(CONF_HOST),
+        config.get(CONF_PORT),
+        config.get(CONF_USERNAME),
+        config.get(CONF_PASSWORD),
+    )
+
+    if not controller.ping():
+        _LOGGER.error("GCE Eco-Devices didn't answer to the request, unable to set up")
+        raise ConfigEntryNotReady
+
+    _LOGGER.info(
+        "Successfully connected to the Eco-Device gateway: %s.",
+        config.get(CONF_HOST, CONF_PORT),
+    )
+
+    if config.get(CONF_USERNAME) and config.get(CONF_PASSWORD):
+        _LOGGER.info(
+            "Authenticated as %s.",
+            config.get(CONF_USERNAME),
+        )
+
+    undo_listener = entry.add_update_listener(_async_update_listener)
+
+    hass.data[DOMAIN][entry.entry_id] = {
+        CONF_NAME: config.get(CONF_NAME),
+        CONTROLLER: controller,
+        CONFIG: config,
+        UNDO_UPDATE_LISTENER: undo_listener,
+    }
+
+    device_registry = await dr.async_get_registry(hass)
+    device_registry.async_get_or_create(
+        config_entry_id=entry.entry_id,
+        identifiers={(DOMAIN, controller.host)},
+        manufacturer="GCE",
+        model="Eco-Devices",
+        name=config.get(CONF_NAME),
+    )
+
+    for component in PLATFORMS:
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(entry, component)
+        )
+
+    return True
+
+
+async def _async_update_listener(hass: HomeAssistant, entry: ConfigEntry):
+    """Handle options update."""
+    await hass.config_entries.async_reload(entry.entry_id)
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    unload_ok = all(
+        await asyncio.gather(
+            *[
+                hass.config_entries.async_forward_entry_unload(entry, component)
+                for component in PLATFORMS
+            ]
+        )
+    )
+
+    hass.data[DOMAIN][entry.entry_id][UNDO_UPDATE_LISTENER]()
+
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
---
+++ custom_components/ecodevices/config_flow.py
@@ -0,0 +1,69 @@
+"""Config flow to configure the eco-devices integration."""
+from homeassistant import config_entries
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
+
+from .const import (
+    DOMAIN,
+    CONTROLLER,
+    CONFIG,
+    CONF_T1_ENABLED,
+    CONF_T2_ENABLED,
+    CONF_C1_ENABLED,
+    CONF_C2_ENABLED,
+    CONF_T1_NAME,
+    CONF_T1_UNIT_OF_MEASUREMENT,
+    CONF_T2_NAME,
+    CONF_T2_UNIT_OF_MEASUREMENT,
+    CONF_C1_NAME,
+    CONF_C2_NAME,
+)
+from homeassistant.const import (
+    CONF_NAME,
+    CONF_HOST,
+    CONF_PORT,
+    CONF_USERNAME,
+    CONF_PASSWORD,
+)
+
+DATA_SCHEMA = vol.Schema(
+    {
+        vol.Optional(CONF_NAME, default="Eco-Devices"): str,
+        vol.Required(CONF_HOST): str,
+        vol.Optional(CONF_PORT, default=80): int,
+        vol.Optional(CONF_USERNAME): str,
+        vol.Optional(CONF_PASSWORD): str,
+        vol.Required(CONF_T1_ENABLED, default=False): bool,
+        vol.Required(CONF_T2_ENABLED, default=False): bool,
+        vol.Required(CONF_C1_ENABLED, default=False): bool,
+        vol.Required(CONF_C2_ENABLED, default=False): bool,
+    }
+)
+
+# @config_entries.HANDLERS.register(DOMAIN)
+class EcodevicesConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a eco-devices config flow."""
+
+    VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
+
+    async def async_step_import(self, user_input=None):
+        """Handle configuration by yaml file."""
+        return await self.async_step_user(user_input)
+
+    async def async_step_user(self, user_input=None):
+        """Handle a flow initialized by the user."""
+        if user_input is not None:
+            entry = await self.async_set_unique_id(
+                f"{DOMAIN}, {user_input.get(CONF_HOST)}"
+            )
+
+            if entry:
+                self.hass.config_entries.async_update_entry(entry, data=user_input)
+                self._abort_if_unique_id_configured()
+
+            return self.async_create_entry(
+                title=user_input.get(CONF_HOST), data=user_input
+            )
+
+        return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA)
