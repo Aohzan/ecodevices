6014639b6c649bc3d7afa34d617c4f731e510397
Matthieu Bourgain <matthieu.bourgain@fr.clara.net>
2021-04-13 00:19:13 +0200

    rewrite with async

--- custom_components/ecodevices/__init__.py
+++ custom_components/ecodevices/__init__.py
@@ -1,24 +1,36 @@
-"""GCE Eco-Devices integration"""
+"""GCE Eco-Devices integration."""
 import asyncio
+from datetime import timedelta
 import logging
 
-import homeassistant.helpers.config_validation as cv
-import voluptuous as vol
-from homeassistant.components.sensor import PLATFORM_SCHEMA
+from pyecodevices import (
+    EcoDevices,
+    EcoDevicesCannotConnectError,
+    EcoDevicesInvalidAuthError,
+)
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
     CONF_HOST,
-    CONF_NAME,
     CONF_PASSWORD,
     CONF_PORT,
+    CONF_SCAN_INTERVAL,
     CONF_USERNAME,
 )
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers import device_registry as dr
-from pyecodevices import EcoDevices
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
-from .const import CONFIG, CONTROLLER, DOMAIN, PLATFORMS, UNDO_UPDATE_LISTENER
+from .const import (
+    CONFIG,
+    CONTROLLER,
+    COORDINATOR,
+    DOMAIN,
+    PLATFORMS,
+    UNDO_UPDATE_LISTENER,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -33,33 +45,50 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up Eco-Devices from a config entry."""
     config = entry.data
 
+    session = async_get_clientsession(hass, False)
+
     controller = EcoDevices(
         config.get(CONF_HOST),
         config.get(CONF_PORT),
         config.get(CONF_USERNAME),
         config.get(CONF_PASSWORD),
+        session=session,
     )
 
-    if not await hass.async_add_executor_job(controller.ping):
-        _LOGGER.error("GCE Eco-Devices didn't answer to the request, unable to set up")
+    try:
+        await controller.get_info()
+    except EcoDevicesCannotConnectError as exception:
+        raise ConfigEntryNotReady from exception
+
+    async def async_update_data():
+        """Fetch data from API."""
+        try:
+            return await controller.global_get()
+        except EcoDevicesInvalidAuthError as err:
+            raise UpdateFailed("Authentication error on Eco-Devices") from err
+        except EcoDevicesCannotConnectError as err:
+            raise UpdateFailed(f"Failed to communicating with API: {err}") from err
+
+    scan_interval = config.get(CONF_SCAN_INTERVAL)
+
+    coordinator = DataUpdateCoordinator(
+        hass,
+        _LOGGER,
+        name=DOMAIN,
+        update_method=async_update_data,
+        update_interval=timedelta(seconds=scan_interval),
+    )
+
+    await coordinator.async_refresh()
+
+    if not coordinator.last_update_success:
         raise ConfigEntryNotReady
-
-    _LOGGER.info(
-        "Successfully connected to the Eco-Device gateway: %s.",
-        config.get(CONF_HOST, CONF_PORT),
-    )
-
-    if config.get(CONF_USERNAME) and config.get(CONF_PASSWORD):
-        _LOGGER.debug(
-            "Authenticated as %s.",
-            config.get(CONF_USERNAME),
-        )
 
     undo_listener = entry.add_update_listener(_async_update_listener)
 
     hass.data[DOMAIN][entry.entry_id] = {
-        CONF_NAME: config.get(CONF_NAME),
         CONTROLLER: controller,
+        COORDINATOR: coordinator,
         CONFIG: config,
         UNDO_UPDATE_LISTENER: undo_listener,
     }
@@ -70,12 +99,14 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
         identifiers={(DOMAIN, controller.host)},
         manufacturer="GCE",
         model="Eco-Devices",
-        name=config.get(CONF_NAME),
+        name=controller.host,
+        sw_version=controller.version,
+        connections={(dr.CONNECTION_NETWORK_MAC, controller.mac_address)},
     )
 
-    for component in PLATFORMS:
+    for platform in PLATFORMS:
         hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, component)
+            hass.config_entries.async_forward_entry_setup(entry, platform)
         )
 
     return True
--- custom_components/ecodevices/config_flow.py
+++ custom_components/ecodevices/config_flow.py
@@ -1,37 +1,51 @@
-"""Config flow to configure the eco-devices integration."""
-import homeassistant.helpers.config_validation as cv
+"""Config flow to configure the GCE Eco-Devices integration."""
+from pyecodevices import (
+    EcoDevices,
+    EcoDevicesCannotConnectError,
+    EcoDevicesInvalidAuthError,
+)
 import voluptuous as vol
+
 from homeassistant import config_entries
+from homeassistant.components.sensor import DEVICE_CLASSES as SENSOR_DEVICE_CLASSES
 from homeassistant.const import (
     CONF_HOST,
-    CONF_NAME,
     CONF_PASSWORD,
     CONF_PORT,
+    CONF_SCAN_INTERVAL,
     CONF_USERNAME,
 )
-from pyecodevices import EcoDevices
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .const import (
+    CONF_C1_DEVICE_CLASS,
     CONF_C1_ENABLED,
     CONF_C1_NAME,
+    CONF_C1_UNIT_OF_MEASUREMENT,
+    CONF_C2_DEVICE_CLASS,
     CONF_C2_ENABLED,
     CONF_C2_NAME,
+    CONF_C2_UNIT_OF_MEASUREMENT,
     CONF_T1_ENABLED,
     CONF_T1_NAME,
     CONF_T1_UNIT_OF_MEASUREMENT,
     CONF_T2_ENABLED,
     CONF_T2_NAME,
     CONF_T2_UNIT_OF_MEASUREMENT,
-    CONFIG,
-    CONTROLLER,
+    DEFAULT_C1_NAME,
+    DEFAULT_C2_NAME,
+    DEFAULT_T1_NAME,
+    DEFAULT_T1_UNIT_OF_MEASUREMENT,
+    DEFAULT_T2_NAME,
+    DEFAULT_T2_UNIT_OF_MEASUREMENT,
     DOMAIN,
 )
 
-DATA_SCHEMA = vol.Schema(
+BASE_SCHEMA = vol.Schema(
     {
-        vol.Optional(CONF_NAME, default="Eco-Devices"): str,
         vol.Required(CONF_HOST): str,
         vol.Optional(CONF_PORT, default=80): int,
+        vol.Required(CONF_SCAN_INTERVAL, default=5): int,
         vol.Optional(CONF_USERNAME): str,
         vol.Optional(CONF_PASSWORD): str,
         vol.Required(CONF_T1_ENABLED, default=False): bool,
@@ -43,42 +57,108 @@ DATA_SCHEMA = vol.Schema(
 
 
 @config_entries.HANDLERS.register(DOMAIN)
-class EcodevicesConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class EcoDevicesConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a eco-devices config flow."""
 
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
 
-    async def async_step_import(self, user_input=None):
-        """Handle configuration by yaml file."""
-        return await self.async_step_user(user_input)
+    def __init__(self):
+        """Initialize class variables."""
+        self.base_input = {}
 
     async def async_step_user(self, user_input=None):
         """Handle a flow initialized by the user."""
         errors = {}
-        if user_input is not None:
-            entry = await self.async_set_unique_id(
-                f"{DOMAIN}, {user_input.get(CONF_HOST)}"
+        if user_input is None:
+            return self.async_show_form(
+                step_id="user", data_schema=BASE_SCHEMA, errors=errors
             )
 
-            if entry:
-                self.hass.config_entries.async_update_entry(entry, data=user_input)
-                self._abort_if_unique_id_configured()
+        entry = await self.async_set_unique_id(f"{DOMAIN}, {user_input.get(CONF_HOST)}")
 
-            controller = EcoDevices(
-                user_input.get(CONF_HOST),
-                user_input.get(CONF_PORT),
-                user_input.get(CONF_USERNAME),
-                user_input.get(CONF_PASSWORD),
+        if entry:
+            self._abort_if_unique_id_configured()
+
+        session = async_get_clientsession(self.hass, False)
+
+        controller = EcoDevices(
+            user_input.get(CONF_HOST),
+            user_input.get(CONF_PORT),
+            user_input.get(CONF_USERNAME),
+            user_input.get(CONF_PASSWORD),
+            session=session,
+        )
+
+        try:
+            await controller.get_info()
+        except EcoDevicesInvalidAuthError:
+            errors["base"] = "invalid_auth"
+            return self.async_show_form(
+                step_id="user", data_schema=BASE_SCHEMA, errors=errors
             )
-
-            if await self.hass.async_add_executor_job(controller.ping):
-                return self.async_create_entry(
-                    title=user_input.get(CONF_HOST), data=user_input
-                )
-
+        except EcoDevicesCannotConnectError:
             errors["base"] = "cannot_connect"
+            return self.async_show_form(
+                step_id="user", data_schema=BASE_SCHEMA, errors=errors
+            )
+
+        self.base_input = user_input
+        return await self.async_step_params()
+
+    async def async_step_params(self, user_input=None):
+        """Handle the param flow to customize the device accordly to enabled inputs."""
+        if user_input is not None:
+            user_input.update(self.base_input)
+            return self.async_create_entry(
+                title=user_input.get(CONF_HOST), data=user_input
+            )
+
+        params_schema = {}
+        if self.base_input.get(CONF_T1_ENABLED):
+            params_schema.update(
+                {
+                    vol.Required(CONF_T1_NAME, default=DEFAULT_T1_NAME): str,
+                    vol.Required(
+                        CONF_T1_UNIT_OF_MEASUREMENT,
+                        default=DEFAULT_T1_UNIT_OF_MEASUREMENT,
+                    ): str,
+                }
+            )
+
+        if self.base_input.get(CONF_T2_ENABLED):
+            params_schema.update(
+                {
+                    vol.Required(CONF_T2_NAME, default=DEFAULT_T2_NAME): str,
+                    vol.Required(
+                        CONF_T2_UNIT_OF_MEASUREMENT,
+                        default=DEFAULT_T2_UNIT_OF_MEASUREMENT,
+                    ): str,
+                }
+            )
+
+        if self.base_input.get(CONF_C1_ENABLED):
+            params_schema.update(
+                {
+                    vol.Required(CONF_C1_NAME, default=DEFAULT_C1_NAME): str,
+                    vol.Required(CONF_C1_DEVICE_CLASS): vol.All(
+                        str, vol.Lower, vol.In(SENSOR_DEVICE_CLASSES)
+                    ),
+                    vol.Optional(CONF_C1_UNIT_OF_MEASUREMENT): str,
+                }
+            )
+
+        if self.base_input.get(CONF_C2_ENABLED):
+            params_schema.update(
+                {
+                    vol.Required(CONF_C2_NAME, default=DEFAULT_C2_NAME): str,
+                    vol.Required(CONF_C2_DEVICE_CLASS): vol.All(
+                        str, vol.Lower, vol.In(SENSOR_DEVICE_CLASSES)
+                    ),
+                    vol.Optional(CONF_C2_UNIT_OF_MEASUREMENT): str,
+                }
+            )
 
         return self.async_show_form(
-            step_id="user", data_schema=DATA_SCHEMA, errors=errors
+            step_id="params", data_schema=vol.Schema(params_schema)
         )
